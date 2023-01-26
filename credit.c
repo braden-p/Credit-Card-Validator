ce87235592589605621838ee68f1be7e180e7954
Braden Piper <bgpiper@gmail.com>
2023-01-26 17:17:07 -0600

    Write program description in C version of program

--- credit.c
+++ credit.c
@@ -1,3 +1,30 @@
+/* Credit Card Validator
+Created by Braden Piper, bradenpiper.com
+Created on Thu Jan 16, 2023
+Version = 1.0
+------------------------------------------
+DESCRIPTION:
+A program that checks the validity of a credit card number using Luhn's Algorithm.
+The program accepts an input of a credit card number, and will calculate if the
+number is syntactically valid using Luhn's Algorithm. In addition to checking the
+validity of the card number, the program will also identify if the credit card number
+is one issued by American Express, Visa, or Mastercard.
+------------------------------------------
+NOTES:
+• This program was made for the class CS50x = Introduction to Computer Science
+offered by Harvard University. It uses the cs50 header file which is not a
+standard header. This header includes the function get_long(), which is used
+to accept an integer input from the user.
+• This program is meant for educational purposes only, and is not meant to be used
+as an actual tool for validating credit card numbers.
+• This program will only work for credit card numbers of American Express, Visa, and
+Mastercard. Credit card numbers issued from other companies will be considered invalid.
+• This program will only work with credit card numbers of length 13, 15, or 16 digits.
+• This program will only work with Mastercard numbers starting in numbers 51, 52,
+53, 54, or 55. */
+
+
+
 #include <cs50.h>
 #include <stdio.h>
 
